59ea6b0d840262cc0dd5aebd25e97704f39365cd
Caio dias <caiodiasmartins12@gmail.com>
2023-12-20 10:15:17 -0300

    BoardException and defensive programming

--- bordeGame/Borde.java
+++ bordeGame/Borde.java
@@ -8,6 +8,9 @@ public class Borde {
     public Borde(){}
 
     public Borde(int linhas, int colunas){
+        if (linhas < 1 || colunas < 1) {
+            throw new BordeException("Erro criando borde: tem que ter pelo menos linha 1 e coluna 1");
+        }
         this.linhasB = linhas;
         this.colunasB = colunas;
         this.pieces = new Piece[linhas][colunas];
@@ -30,15 +33,39 @@ public class Borde {
     }
 
     public Piece pieces(int linha,int coluna){
+        if (!positionExists(linha, coluna)) {
+            throw new BordeException("Position not on the board");
+        }
         return pieces[linha][coluna];
     }
 
     public Piece pieces(Position position){
+        if (!positionExists(position)) {
+            throw new BordeException("Position not on the board");
+        }
         return pieces[position.getLinha()][position.getColuna()];
     }
 
     public void placePieces(Piece piece,Position position){
+        if (ThereIsAPiece(position)) {
+            throw new BordeException("There is already a piece on position " + position);
+        }
         this.pieces[position.getLinha()][position.getColuna()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int linhas, int colunas ){
+      return linhas >= 0 && linhas < this.linhasB && colunas >= 0 && colunas < this.colunasB;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getLinha(), position.getColuna());
+    }
+
+    public boolean ThereIsAPiece(Position position){
+        if (!positionExists(position)) {
+            throw new BordeException("Position not on the board");
+        }
+       return pieces(position) != null;
     }
 }
--- bordeGame/BordeException.java
+++ bordeGame/BordeException.java
@@ -2,6 +2,8 @@ package Sistemajogodexadrez.bordeGame;
 
 public class BordeException extends RuntimeException {
     
+    private static final long serialVersionUID = 1L;
+
     public BordeException(String msg){
         super(msg);
     }
