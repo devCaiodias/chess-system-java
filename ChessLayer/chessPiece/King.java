a6e8eb1bf801482438c5daea5aea8e40e815c815
Caio dias <caiodiasmartins12@gmail.com>
2023-12-25 18:17:31 -0300

    Implementing possible moves of King

--- ChessLayer/chessPiece/King.java
+++ ChessLayer/chessPiece/King.java
@@ -3,6 +3,7 @@ package Sistemajogodexadrez.ChessLayer.chessPiece;
 import Sistemajogodexadrez.ChessLayer.ChessPiece;
 import Sistemajogodexadrez.ChessLayer.Color;
 import Sistemajogodexadrez.bordeGame.Borde;
+import Sistemajogodexadrez.bordeGame.Position;
 
 public class King extends ChessPiece {
 
@@ -15,9 +16,66 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMoves(Position position){
+        ChessPiece p = (ChessPiece)getBorde().pieces(position);
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] PossibleMoves() {
         boolean[][] mat = new boolean[getBorde().getLinha()][getBorde().getColunas()];
+
+        Position p = new Position(0, 0);
+
+        //above
+        p.setValues(position.getLinha() - 1, position.getColuna());
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            
+        }
+        
+        //below
+        p.setValues(position.getLinha() + 1, position.getColuna());
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //Left
+        p.setValues(position.getLinha(), position.getColuna() - 1);
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //rigth
+        p.setValues(position.getLinha(), position.getColuna() + 1);
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //nw
+        p.setValues(position.getLinha() - 1, position.getColuna() - 1);
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+        
+        //ne
+        p.setValues(position.getLinha() - 1, position.getColuna() + 1);
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //sw
+        p.setValues(position.getLinha() + 1, position.getColuna() - 1);
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+         //sw
+        p.setValues(position.getLinha() + 1, position.getColuna() + 1);
+        if (getBorde().positionExists(p) && canMoves(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
         return mat;
     }
     
